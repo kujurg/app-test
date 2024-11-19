d1fc871c9e3e385cdfcd64affafc7c27eb6d3cef
gladson <gladsonkuju99@gmail.com>
2024-11-19 03:39:26 +0000

    iam

--- terraform/app_infra/iam.tf
+++ terraform/app_infra/iam.tf
@@ -1,16 +1,19 @@
 
-# resource "google_project_iam_member" "gke" {
-#   project = var.project_id
-#   role = "roles/artifactregistry.reader"
-#   member = "serviceAccount:${google_service_account.gke.email}"
-# }
-
-resource "google_artifact_registry_repository_iam_binding" "gke" {
-  repository = var.artifact_registory_name
-  location   = var.region
-  role       = "roles/artifactregistry.reader"
-
-  members = [
-    "serviceAccount:${google_service_account.gke.email}"
-  ]
+resource "google_project_iam_member" "gke" {
+  project = var.project_id
+  role = "roles/artifactregistry.reader"
+  member = "serviceAccount:${google_service_account.gke.email}"
 }
+
+# resource "google_artifact_registry_repository_iam_binding" "gke" {
+#   repository = var.artifact_registory_name
+#   location   = var.region
+#   role       = "roles/artifactregistry.reader"
+
+#   members = [
+#     "serviceAccount:${google_service_account.gke.email}"
+#   ]
+#    depends_on = [
+#     azurerm_storage_account.st
+#  ]
+# }
